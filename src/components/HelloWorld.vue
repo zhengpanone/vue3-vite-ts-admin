e1e53df7942aff8c9765deba178aa0d88ce1d009
zhengpanone <zhengpanone@hotmail.com>
2022-10-11 00:22:47 +0800

    feat: 项目脚手架搭建

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,16 @@
   defineProps<{ msg: string }>()
 
   const count = ref(0)
+  const add = () => {
+    count.value += 2
+  }
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="add">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
